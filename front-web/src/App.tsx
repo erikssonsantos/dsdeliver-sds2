c15df8b2856c506d81f3e29741521113fa080545
Eriksson Santos <erikssonsantos.py@gmail.com>
2021-01-07 18:05:25 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   ) 
 }
 
