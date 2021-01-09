c3e9f2cade0eed0a1ce89f0017e4a2d311a8e382
Eriksson Santos <erikssonsantos.py@gmail.com>
2021-01-09 20:53:45 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import Routes from './Routes';
 
@@ -7,7 +10,10 @@ import Routes from './Routes';
 
 function App() {
   return (
+    <>
     <Routes />
+    <ToastContainer />
+    </>
   ) 
 }
 
